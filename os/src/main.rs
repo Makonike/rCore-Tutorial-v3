0a0b16fe64933438b0e4e7cb15c15af5585c68a1
xiaodongzhang <xiaodongzhang@whale.im>
2023-04-26 08:52:45 +0000

    1.4

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
+
+use core::arch::global_asm;
 mod lang_items;
 // fn main() {
 // println!("Hello, world!");
 // }
+global_asm!(include_str!("entry.asm")); // 将汇编代码转为字符串，通过宏嵌入到代码中
