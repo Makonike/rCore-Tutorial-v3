0d1f77c86f88d1983fecaf2e6c85cf08747783e1
xiaodongzhang <xiaodongzhang@whale.im>
2023-07-31 18:24:55 +0800

    feat: ch1 sbi

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,10 +1,32 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+#[macro_use]
+mod console;
 mod lang_items;
+mod sbi;
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
 // fn main() {
-// println!("Hello, world!");
+//     println!("Hello, world!");
 // }
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, rCore!");
+    panic!("shutdown machine");
+}
+
+// clear bss segment data
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) };
+    })
+}
